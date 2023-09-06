84a7b76f29308ecc496017d42bae7f83a044fc68
VsPot07 <cocoacake84@gmail.com>
2023-09-06 23:24:50 +0900

    0906

--- Assets/Scripts/Ware/Rook.cs
+++ Assets/Scripts/Ware/Rook.cs
@@ -6,6 +6,17 @@ public class Rook : WareBase
 {
     public override void LookCanMoveBlock()
     {
+        Transform trm = MapManager.Instance.MapDataParent;
 
+        for (int i = 0; i < trm.childCount; i++)
+        {
+            string map = trm.GetChild(i).name;
+            if((map.Contains(CurrentPos[0]) ||
+                map.Contains(CurrentPos[1])) && map != CurrentPos)
+            {
+                _blockMarkSpawner.MarkSpawn(transform, trm.GetChild(i), true, map);
+            }
+        }
+        
     }
 }
--- Assets/Scripts/Ware/WareBase.cs
+++ Assets/Scripts/Ware/WareBase.cs
@@ -6,14 +6,14 @@ using UnityEngine.Events;
 public abstract class WareBase : MonoBehaviour
 {
     private ClickObserver _wareClickObserver;
-    public bool isSelected;
+    public bool isSelected; 
     public string CurrentPos;
     [SerializeField] protected WareType _wareType;
     [SerializeField] protected bool _isBlack;
     protected BlockMarkSpawner _blockMarkSpawner;
 
-    protected string _mapMarkCharData = "ABCDEFGH";
-    protected int[] _mapMarkIntData = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    protected string _mapMarkCharData = "ABCDEFGHXXXXXX";
+    protected int[] _mapMarkIntData = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 
     [SerializeField] private Material[] _activeMatArr = new Material[2];
     [SerializeField] private Material[] _removeMatArr = new Material[2];
