b3b58c7ae28c05a0e154ffa3d5a8e6053b769a99
VsPot07 <cocoacake84@gmail.com>
2023-09-11 17:50:19 +0900

    0911

--- Assets/Scripts/Core/WareManager.cs
+++ Assets/Scripts/Core/WareManager.cs
@@ -10,12 +10,12 @@ public class WareManager : ObjectManager
     public List<GameObject> WarePrefabs = new List<GameObject>();
     public List<Sprite> WareSprites = new List<Sprite>();
 
-    public Camera MainCam;
+    
 
     public override void SetInstance()
     {
         Instance = this;
-        MainCam = Camera.main;
+        
     }
 
     public void ReadyToAttack()
@@ -26,7 +26,7 @@ public class WareManager : ObjectManager
     public void CreateWare(WareType wType, bool isBlack, string ID)
     {
         Transform trm = MapManager.Instance.MapDatas[ID];
-        SelectWare = Instantiate(isBlack ? WarePrefabs[(int)wType] : WarePrefabs[(int)wType + 6]).GetComponent<WareBase>();
+        SelectWare = Instantiate(!isBlack ? WarePrefabs[(int)wType] : WarePrefabs[(int)wType + 6]).GetComponent<WareBase>();
         SelectWare.transform.position = new Vector3(trm.position.x, 25, trm.position.z);
         SelectWare.CurrentPos = ID;
 
@@ -34,6 +34,5 @@ public class WareManager : ObjectManager
         seq.Join(SelectWare.transform.DOMoveY(10, 0.8f).SetEase(Ease.InExpo));
         seq.Join(SelectWare.transform.DOScale(new Vector3(5, 5, 5), 0.8f).SetEase(Ease.InExpo));
         EffectManager.Instance.CreateFX(trm, FXType.dust, 0.7f);
-
     }
 }
--- Assets/Scripts/Core/WeaponManager.cs
+++ Assets/Scripts/Core/WeaponManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class WeaponManager : ObjectManager
 {
@@ -8,9 +9,31 @@ public class WeaponManager : ObjectManager
     public WeaponBase SelectWeapon;
     [SerializeField] private List<WeaponInfo> _weaponInfos = new List<WeaponInfo>();
 
+    public Camera MainCam;
+
+    private bool _isActiveShoulderView;
+
     public override void SetInstance()
     {
         Instance = this;
+        MainCam = Camera.main;
+    }
+
+    public void ActiveShoulderView()
+    {
+        if(!_isActiveShoulderView)
+        {
+            MainCam.transform.parent = WareManager.Instance.SelectWare.transform;
+            MainCam.transform.DOLocalMove(new Vector3(2.7f, 4, -3.5f), 0.7f);
+            MainCam.transform.DOLocalRotate(new Vector3(26.5f, -44f, 0), 0.7f);
+        }
+        else
+        {
+            MainCam.transform.parent = null;
+            MainCam.transform.DOMove(new Vector3(54.7f, 85.8f, -61.2f), 0.7f);
+            MainCam.transform.DORotate(new Vector3(50.9f, -44f, 0), 0.7f);
+        }
+        _isActiveShoulderView = !_isActiveShoulderView;
     }
 
     public void EquipWeapon(WeaponType wType, Transform parent)
@@ -28,6 +51,7 @@ public class WeaponManager : ObjectManager
         if(Input.GetKeyDown(KeyCode.Return))
         {
             WareManager.Instance.ReadyToAttack();
+            ActiveShoulderView();
         }
     }
 }
